77c07d0fa8193001b2552ba1509e1193604c6f3f
Fernando Campos <fhcs2630@gmail.com>
2018-07-09 15:03:32 -0300

    criando page admin

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 
 require_once("vendor/autoload.php");
 use \Slim\Slim;
-use \Hcode\page;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+
+
 $app = new Slim();
 
 $app->config('debug', true);
@@ -12,6 +15,18 @@ $app->get('/', function() {
 	$page =  new Page();
 
 	$page->setTpl("index");
+
+	
+
+});
+
+$app->get('/admin', function() {
+    
+	$page =  new PageAdmin();
+
+	$page->setTpl("index");
+
+	
 
 });
 
