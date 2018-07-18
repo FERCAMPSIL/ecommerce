f82a51222563ec26dff3670afc56e20bd980db25
Fernando Campos <fhcs2630@gmail.com>
2018-07-18 15:53:13 -0300

    paginaçao

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -90,6 +90,30 @@ public function getProducts($related=true){
         }
     }
 
+    public function getProductsPage($page=1, $itemsPerPage = 3){
+
+        $start=($page - 1)* $itemsPerPage;
+        $sql = new Sql();
+        $results = $sql->select("
+                    SELECT SQL_CALC_FOUND_ROWS *
+                        from tb_products a      
+                        INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+                        INNER JOIN  tb_categories c ON c.idcategory = b.idcategory
+                        WHERE c.idcategory = :idcategory
+                        LIMIT $start, $itemsPerPage;
+                        ",[
+                            'idcategory'=>$this->getidcategory()
+                        ]);
+
+           $resultsTotal= $sql->select(" SELECT FOUND_ROWS() AS nrtotal;");
+
+           return [
+               'data'=>Product::checkList($results),
+               'total'=>(int)$resultsTotal[0]["nrtotal"],
+               'pages'=>ceil($resultsTotal[0]["nrtotal"]/$itemsPerPage)
+           ];
+    }
+
     public function addProduct(Product $product)
 	{
 		$sql = new Sql();
@@ -106,6 +130,8 @@ public function getProducts($related=true){
 			':idcategory'=>$this->getidcategory(),
 			':idproduct'=>$product->getidproduct()
 		]);
-	}
+    }
+    
+  
 }
 ?>
