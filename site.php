0315b33329f29f7db1e2731abdf9181047fc9fc6
Fernando Campos <fhcs2630@gmail.com>
2018-07-20 17:03:22 -0300

    carrinho calculo de frete

--- site.php
+++ site.php
@@ -52,6 +52,62 @@ $app->get("/products/:desurl", function($desurl){
 $app->get("/cart", function(){
 	$cart = Cart::getFromSession();
 	$page = new Page();
-	$page->setTpl("cart");
+	$page->setTpl("cart", [
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
+	]);
 });
+
+$app->get("/products/:desurl", function($desurl){
+
+	$product = new Product();
+	$product->get((int)$idproduct);
+
+});
+
+$app->get("/cart/:idproduct/add", function($idproduct){
+
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$cart = Cart::getFromSession();
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+	for($i = 0; $i< $qtd; $i++){
+		$cart->addProduct($product);
+	}
+	header("location:/cart");
+	exit;
+});
+
+$app->get("/cart/:idproduct/minus", function($idproduct){
+
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$cart = Cart::getFromSession();
+	$cart->removeProduct($product);
+
+	header("location:/cart");
+	exit;
+});
+
+$app->get("/cart/:idproduct/remove", function($idproduct){
+
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$cart = Cart::getFromSession();
+	$cart->removeProduct($product,true);
+
+	header("location:/cart");
+	exit;
+});
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+	$cart->setFreight($_POST['zipcode']);
+
+	header("location:/cart");
+	exit;
+});
+
 ?>
